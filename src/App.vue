446ad96db3580d9a56092f73d72b56c214ea318f
FriyadDev <riyadhasanblue2323@gmail.com>
2025-08-10 21:03:03 +0600

    refactor: update navigation to dynamically generate links from router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import router from './router'
 </script>
 
 <template>
-  <header class="block w-full bg-black px-10 py-4">
+  <header class="block w-full bg-black px-10 py-4 mb-4">
     <nav class="flex justify-start items-center gap-8">
       <RouterLink
-        to="/"
-        class="aria-[current='page']:text-green-500 aria-[current='page']:underline"
-        >Home</RouterLink
-      >
-      <RouterLink
-        to="/about"
-        class="aria-[current='page']:text-green-500 aria-[current='page']:underline"
-        >About</RouterLink
+        v-for="link in router.getRoutes()"
+        :key="link.name"
+        :to="link.path"
+        class="aria-[current='page']:text-green-500 aria-[current='page']:underline capitalize"
+        >{{ link.name }}</RouterLink
       >
     </nav>
   </header>
 
-  <div class="">
-    <RouterView />
-  </div>
+  <RouterView />
 </template>
-
-<style scoped>
-/* asdfsd */
-</style>
