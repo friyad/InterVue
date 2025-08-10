f7ba2f17186362d6feab89bf88989190850e627b
FriyadDev <riyadhasanblue2323@gmail.com>
2025-08-10 20:59:18 +0600

    feat: use Options and Composition API together in About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,65 @@
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  data() {
+    return {
+      answer: 'Is it going to rain today?',
+      loading: false,
+    }
+  },
+  setup() {
+    let pageName = ref('About')
+    const changeRandomName = () => {
+      const randomNumber = Math.floor(Math.random() * 100)
+      pageName.value = `About: ${randomNumber}`
+    }
+    return { pageName, changeRandomName }
+  },
+  methods: {
+    async getAnswer() {
+      this.loading = true
+      this.answer = 'Thinking...'
+      try {
+        const res = await fetch('https://yesno.wtf/api')
+        this.answer = (await res.json()).answer
+      } catch (error) {
+        this.answer = 'Error! Could not reach the API. ' + error
+      } finally {
+        this.loading = false
+      }
+    },
+  },
+}
+</script>
+
 <template>
+  <p class="mt-4 mb-8">
+    <strong>This page uses Options API and Composition API together.</strong>
+    <br />
+    The Options API is used for the methods and data, while the Composition API is used for the
+    reactive state of the page name.
+  </p>
+
   <div>
-    <h1 class="text-4xl">This is an about page</h1>
+    <h1 @click="getAnswer" class="text-4xl">{{ answer }}</h1>
+    <button
+      @click="getAnswer"
+      class="px-4 py-2.5 text-white bg-amber-800 rounded-lg mt-4 cursor-pointer"
+    >
+      {{ loading ? 'Loading...' : 'Get Answer (Option)' }}
+    </button>
+  </div>
+
+  <div class="mt-10">
+    <h1 @click="changeRandomName" class="text-4xl">This is an {{ pageName }} page</h1>
+    <button
+      @click="changeRandomName"
+      class="px-4 py-2.5 text-white bg-blue-800 rounded-lg mt-4 cursor-pointer"
+    >
+      Change Page Name (Composition)
+    </button>
   </div>
 </template>
 
-<style>
-/* @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-} */
-</style>
+<style></style>
