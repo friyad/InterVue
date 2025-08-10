6ff938324063d7bd8bad84821e1e0dd82ad99de3
FriyadDev <riyadhasanblue2323@gmail.com>
2025-08-10 22:05:03 +0600

    feat: add Class Binding page with reactive count example

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ClassBinding from '@/views/ClassBinding.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/class-binding',
+      name: 'Class Binding',
+      component: ClassBinding,
     },
   ],
 })
---
+++ src/views/ClassBinding.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue'
+
+const hello = reactive({
+  count: 4,
+})
+
+const updateCount = () => {
+  hello.count += 5
+}
+const seen = true
+</script>
+
+<template>
+  <main>
+    <p>
+      <strong>This page shows: Class Binding</strong>
+    </p>
+    <p
+      v-if="seen"
+      @click="updateCount"
+      class="text-2xl my-4 select-none"
+      :class="{
+        'text-blue-500': hello.count < 10,
+        'text-green-500': hello.count >= 10,
+        'text-red-500': hello.count > 20,
+        'text-yellow-500': hello.count > 30,
+        'text-purple-500': hello.count > 40,
+      }"
+    >
+      {{ hello.count }}: Click me. I will change. I show the class Bindings.
+    </p>
+  </main>
+</template>
